6c431c9f7510620e0c0bf3d1534157cba362c955
冯辉 <fenghui@iscs.com.cn>
2016-07-13 14:56:25 +0800

    stackoverflow error, Thank you help me to solve the problem。

--- app/src/main/java/com/himan/himanpro/fragment/GanHuoFragment.java
+++ app/src/main/java/com/himan/himanpro/fragment/GanHuoFragment.java
@@ -68,29 +68,28 @@ import java.util.List;
 /**
  * Created by HIMan on 16/7/4.
  */
-//public class GanHuoFragment extends BaseFragment implements ISetLoad, SwipeRefreshLayout.OnRefreshListener {
-public class GanHuoFragment extends BaseFragment {
+public class GanHuoFragment extends BaseFragment implements ISetLoad, SwipeRefreshLayout.OnRefreshListener {
 
     private View view;
-//    private RandomDataPresemter randomPresenter;
-//    private ListView ganhuo_lv;
-//    private List<RandomData.ResultsBean> resultsList;
-//    private GanHuoAdapter adapter;
-//    private SwipeRefreshLayout swip_layout;
-//    private int i = 2;
-//    public String url;
-//    private Handler handler = new Handler(){
-//        @Override
-//        public void handleMessage(Message msg) {
-//            switch(msg.what){
-//                case 1:
-//                    swip_layout.setRefreshing(false);
-//                    Toast.makeText(getActivity(), "刷新完成", Toast.LENGTH_SHORT).show();
-//                    break;
-//            }
-//            super.handleMessage(msg);
-//        }
-//    };
+    private RandomDataPresemter randomPresenter;
+    private ListView ganhuo_lv;
+    private List<RandomData.ResultsBean> resultsList;
+    private GanHuoAdapter adapter;
+    private SwipeRefreshLayout swip_layout;
+    private int i = 2;
+    public String url;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what){
+                case 1:
+                    swip_layout.setRefreshing(false);
+                    Toast.makeText(getActivity(), "刷新完成", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
 
 
@@ -112,9 +111,9 @@ public class GanHuoFragment extends BaseFragment {
     public void onStart() {
         super.onStart();
         LogUtils.i("onStart");
-//        url = ProConstant.getRandomData("10", "1");
-//        randomPresenter = new RandomDataPresemter(this);
-//        randomPresenter.loadRandomData();
+        url = ProConstant.getRandomData("10", "1");
+        randomPresenter = new RandomDataPresemter(this);
+        randomPresenter.loadRandomData();
     }
 
     @Override
@@ -136,100 +135,100 @@ public class GanHuoFragment extends BaseFragment {
     }
 
     public void initView(){
-//        ganhuo_lv = (ListView) view.findViewById(R.id.ganhuo_lv);
-//        swip_layout = (SwipeRefreshLayout) view.findViewById(R.id.swip_layout);
+        ganhuo_lv = (ListView) view.findViewById(R.id.ganhuo_lv);
+        swip_layout = (SwipeRefreshLayout) view.findViewById(R.id.swip_layout);
     }
 
     @Override
     public void initData() {
-//        adapter = new GanHuoAdapter(getActivity());
-//        swip_layout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_orange_dark,
-//                android.R.color.holo_green_light, android.R.color.holo_red_dark);
-//        swip_layout.setOnRefreshListener(this);
+        adapter = new GanHuoAdapter(getActivity());
+        swip_layout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_orange_dark,
+                android.R.color.holo_green_light, android.R.color.holo_red_dark);
+        swip_layout.setOnRefreshListener(this);
     }
 
-//    @Override
-//    public void setUrl(String url) {
-//        this.url = url;
-//    }
-//
-//    @Override
-//    public String getUrl() {
-//        LogUtils.i("url-------->"+url);
-//        return url;
-//    }
-//
-//    @Override
-//    public void showProgress() {
-//        ProgressUtils.showNoteBackProgressDialog(getActivity(), "正在加载");
-//    }
-//
-//    @Override
-//    public void hideProgress() {
-//        ProgressUtils.dismissDialog();
-//    }
-//
-//    @Override
-//    public void successFor(List<RandomData.ResultsBean> randomData) {
-//        resultsList = randomData;
-//        Toast.makeText(getActivity(), "load success", Toast.LENGTH_SHORT).show();
-//        adapter.setList(resultsList);
-//        ganhuo_lv.setAdapter(adapter);
-//        setItemClick();
-//    }
-//
-//    public void setItemClick(){
-//        ganhuo_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                LogUtils.i( "item:"+i+"click.s");
-//                Intent intent = new Intent(getActivity(), ContentActivity.class);
-//                intent.putExtra("content", resultsList.get(i).getContent());
-//                startActivity(intent);
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public void errorFor(String errorInfo) {
-//        Toast.makeText(getActivity(), "load error", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onRefresh() {
-//         setUrl(ProConstant.getRandomData("10", i+""));
-//         randomPresenter.loadRandomData();
-//         i++;
-//        Message msg = Message.obtain();
-//        msg.what = 1;
-//        handler.sendMessage(msg);
-//    }
-//
-//
-//    class GanHuoAdapter extends CustomBaseAdapter<RandomData.ResultsBean>{
-//
-//        GanHuoHolder ganHuoHolder;
-//
-//        public GanHuoAdapter(Context context) {
-//            super(context);
-//        }
-//
-//        @Override
-//        public View setConvertView(int position, View convertView, ViewGroup parent) {
-//            if(convertView == null){
-//                ganHuoHolder = new GanHuoHolder();
-//                convertView = View.inflate(getActivity(), R.layout.ganhuo_lv_items, null);
-//                ganHuoHolder.tv_title = (TextView) convertView.findViewById(R.id.tv_ganhuo_titme);
-//                ganHuoHolder.tv_time = (TextView) convertView.findViewById(R.id.tv_ganhuo_time);
-//                convertView.setTag(ganHuoHolder);
-//            }else{
-//                ganHuoHolder = (GanHuoHolder) convertView.getTag();
-//            }
-//            String time = resultsList.get(position).getPublishedAt();
-//            time = time.substring(0, 10);
-//            ganHuoHolder.tv_title.setText(resultsList.get(position).getTitle());
-//            ganHuoHolder.tv_time.setText(time);
-//            return convertView;
-//        }
-//    }
+    @Override
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String getUrl() {
+        LogUtils.i("url-------->"+url);
+        return url;
+    }
+
+    @Override
+    public void showProgress() {
+        ProgressUtils.showNoteBackProgressDialog(getActivity(), "正在加载");
+    }
+
+    @Override
+    public void hideProgress() {
+        ProgressUtils.dismissDialog();
+    }
+
+    @Override
+    public void successFor(List<RandomData.ResultsBean> randomData) {
+        resultsList = randomData;
+        Toast.makeText(getActivity(), "load success", Toast.LENGTH_SHORT).show();
+        adapter.setList(resultsList);
+        ganhuo_lv.setAdapter(adapter);
+        setItemClick();
+    }
+
+    public void setItemClick(){
+        ganhuo_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                LogUtils.i( "item:"+i+"click.s");
+                Intent intent = new Intent(getActivity(), ContentActivity.class);
+                intent.putExtra("content", resultsList.get(i).getContent());
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void errorFor(String errorInfo) {
+        Toast.makeText(getActivity(), "load error", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRefresh() {
+         setUrl(ProConstant.getRandomData("10", i+""));
+         randomPresenter.loadRandomData();
+         i++;
+        Message msg = Message.obtain();
+        msg.what = 1;
+        handler.sendMessage(msg);
+    }
+
+
+    class GanHuoAdapter extends CustomBaseAdapter<RandomData.ResultsBean>{
+
+        GanHuoHolder ganHuoHolder;
+
+        public GanHuoAdapter(Context context) {
+            super(context);
+        }
+
+        @Override
+        public View setConvertView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                ganHuoHolder = new GanHuoHolder();
+                convertView = View.inflate(getActivity(), R.layout.ganhuo_lv_items, null);
+                ganHuoHolder.tv_title = (TextView) convertView.findViewById(R.id.tv_ganhuo_titme);
+                ganHuoHolder.tv_time = (TextView) convertView.findViewById(R.id.tv_ganhuo_time);
+                convertView.setTag(ganHuoHolder);
+            }else{
+                ganHuoHolder = (GanHuoHolder) convertView.getTag();
+            }
+            String time = resultsList.get(position).getPublishedAt();
+            time = time.substring(0, 10);
+            ganHuoHolder.tv_title.setText(resultsList.get(position).getTitle());
+            ganHuoHolder.tv_time.setText(time);
+            return convertView;
+        }
+    }
 }
